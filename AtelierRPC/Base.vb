e5efe760ef125efc8c4c749c03ce196337012473
FrancoisSaura <franois.saura@gmail.com>
2022-03-11 14:08:17 +0100

    Commit PC Portable

--- AtelierRPC/Base.vb
+++ AtelierRPC/Base.vb
@@ -1,9 +1,14 @@
 ﻿Imports Newtonsoft.Json
+Imports System.Net
+Imports System.Net.Sockets
 Module Base
     Public Class Formulaire
         Public Property Nom As String = Nothing
         Public Property Prenom As String = Nothing
         Public Property Age As Integer = 0
+
+        'Lien du dépot du projet:
+        'https://github.com/FrancisEpsi/AtelierRPC
 
         Public Function GetJson() As String
             Try
@@ -13,13 +18,86 @@ Module Base
             End Try
         End Function
 
-        Public Function LoadJson(ByVal JsonStr As String) As String
-            Dim tempObj = Nothing
+        Public Function LoadJson(ByVal JsonStr As String) As Boolean
+            Dim tempObj As Formulaire = Nothing
             Try
                 tempObj = JsonConvert.DeserializeObject(Of Formulaire)(JsonStr)
+                With tempObj
+                    Me.Nom = tempObj.Nom
+                    Me.Prenom = tempObj.Prenom
+                    Me.Age = tempObj.Age
+                End With
+                Return True
             Catch ex As Exception
-
+                Return False
             End Try
         End Function
+    End Class
+
+    Public Class ClientRPC
+        Public Property ServerIP As String = "127.0.0.1"
+        Public Property ServerPort As Integer = 3426
+        Dim sockClient As New Socket(SocketType.Stream, ProtocolType.Tcp)
+        Dim serverEndPoint As New IPEndPoint(IPAddress.Parse(ServerIP), ServerPort)
+
+        Public Function Connect() As Boolean
+            sockClient = New Socket(SocketType.Stream, ProtocolType.Tcp)
+            Try
+                sockClient.Connect(serverEndPoint)
+                Return True
+            Catch ex As Exception
+                Return False
+            End Try
+        End Function
+
+        Public Function Disconnect() As Boolean
+            Try
+                sockClient.Disconnect(False)
+                Return True
+            Catch ex As Exception
+                Return False
+            End Try
+        End Function
+
+        Public Function SendMesage(ByVal JsonStr As String) As Boolean
+            If Me.Connect() = True Then
+                Try
+                    Dim buffer As Byte() = System.Text.Encoding.UTF8.GetBytes(JsonStr)
+                    sockClient.Send(buffer)
+                    Me.Disconnect()
+                    Return True
+                Catch ex As Exception
+                    Me.Disconnect()
+                    Return False
+                End Try
+            Else
+                Return False
+            End If
+        End Function
+    End Class
+
+    Public Class ServerRPC
+        Dim socketListenner As New Socket(SocketType.Stream, ProtocolType.Tcp)
+        Dim interfaceEcoute As New IPEndPoint(IPAddress.Any, 3426)
+
+        Dim threadListenning As Threading.Thread
+        Dim Listenning As Boolean = False
+
+        Public Sub StartServer()
+            Listenning = True
+            threadListenning = New Threading.Thread(AddressOf Listen)
+            threadListenning.Start()
+
+        End Sub
+
+        Public Sub StopServer()
+
+        End Sub
+
+        Private Sub Listen()
+            While Listenning
+                Dim sockSession = ""
+            End While
+        End Sub
     End Class
 End Module
--- AtelierRPC/PageClient.vb
+++ AtelierRPC/PageClient.vb
@@ -1,10 +1,22 @@
 ﻿Imports Newtonsoft.Json
 Public Class PageClient
+    Dim client As New ClientRPC
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Function GenerateJSON(ByVal FirstName As String, ByVal LastName As String, ByVal Age As Integer)
+    Sub Submit()
+        Dim leFormulaire = New Formulaire
+        With leFormulaire
+            'Remplacer les "" par les noms des contrôles textbox adéquat:
+            .Nom = ""
+            .Prenom = ""
+            .Age = 0
+        End With
 
-    End Function
+        Dim JsonFormulaire As String = leFormulaire.GetJson
+        If JsonFormulaire Is Nothing Then MsgBox("Il y a eu une erreur de sérialisaiton JSON !", vbCritical) : Exit Sub
+
+    End Sub
 End Class
